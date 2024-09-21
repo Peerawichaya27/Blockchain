19cfac6e034a6e7ace6936735ea67a2c1ac10aa8
Sorravich R. <soravich45@gmail.com>
2024-09-21 20:27:43 +0700

    updated blockchain

--- vc_hash.py
+++
@@ -1,9 +0,0 @@
-import hashlib
-
-# Example student ID (VC)
-student_id = "123456789"
-
-# Compute the SHA256 hash of the student ID
-vc_hash = hashlib.sha256(student_id.encode()).hexdigest()
-
-print(f"0x{vc_hash}")
